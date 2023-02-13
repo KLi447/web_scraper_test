023560473ed428df6841a3cec331a674c26ea47e
KLi447 <kevinchenli44@gmail.com>
2023-02-13 13:24:46 -0500

    Initial connection to MongoDB

--- server/server.js
+++ server/server.js
@@ -1,24 +1,19 @@
-const axios = require('axios')
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config({ path: "./config.env" });
+const port = process.env.PORT || 4000;
+app.use(cors());
+app.use(express.json());
+app.use(require("./routes/record"));
+// get driver connection
+const dbo = require("./db/conn");
 
-const getSingleStockInfo = stock => new Promise((resolve, reject) => {
-    if (!stock) { return reject(Error('Stock symbol required')); }
-    if (typeof stock !== 'string') { return reject(Error(`Invalid argument type. Required: string. Found: ${typeof stock}`)); }
-    
-    // original
-    //const url = `https://query2.finance.yahoo.com/v7/finance/quote?symbols=${stock}`;    
-    // new stock for time period -> chart -> result
-    //const url = "https://query2.finance.yahoo.com/v8/finance/chart/AAPL?symbol=AAPL&period1=1644712262&period2=1676251092&interval=3mo";
-    //new options -> optionChain -> result
-    //const url = "https://query2.finance.yahoo.com/v7/finance/options/AAPL";
-    //new modules -> quoteSummary -> result
-    // const url = "https://query2.finance.yahoo.com/v10/finance/quoteSummary/AAPL?modules=price%2CsectorTrend"
-    return axios.get(url).then((res) => {
-        if (!res) {
-            return resolve(new Error(`Error retrieving info for symbol ${stock}`));
-        }
-        console.log(res.data.quoteSummary.result);
-        return resolve(res);
-    }).catch(err => reject(err));
+app.listen(port, () => {
+  // perform a database connection when server starts
+  dbo.connectToServer(function (err) {
+    if (err) console.error(err);
+
+  });
+  console.log(`Server is running on port: ${port}`);
 });
-
-// getSingleStockInfo("AAPL");
